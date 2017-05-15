8288f5bad5015d27277d04bf332ca9ec018442e7
AasthaShree <aastha.shree@gmail.com>
2017-05-15 05:21:10 +0100

    ProgrammingAssignment2: Lexical Scoping
    R function to cache the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Cache the inverse of a matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## 1. Function to create 'special' matrix to cache its inverse
+makeCacheMatrix <- function(mtx = matrix()) #assuming symmetrix matrix 
+  {
+  inv <- NULL
+  set <- function(tmp){ 
+    mtx <<- tmp
+    inv <<- NULL
+  }
+  get <- function() mtx                     #get the matrix
+  setinv <- function(solve) inv <<- solve   #set the value of inverse of the matrix using solve function
+  getinv <- function() inv                  #get the value of inverse of the matrix
+  list(set = set, get = get,                #create a list of the assigned values
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## 2. Function to comput inverse of 'special' matrix and, in case already calculated, retrieve the cached value
+cacheSolve <- function(mtx = matrix(), ...) {
+  inv <- mtx$getinv()
+  if(!is.null(inv)){                        #check whether the inverse has been computed
+    message("getting cached data")
+    return(inv)
+  }
+  inv_mat <- mtx$get()                      
+  inv <- solve(inv_mat, ...)                #else, compute the inverse matrix
+  mtx$setinv(inv)
+  inv                                       #print the inverse of the matrix
 }
