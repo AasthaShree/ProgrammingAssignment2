348c76fe9312edbe784e2b65342efde3487e94ea
AasthaShree <aastha.shree@gmail.com>
2017-05-15 21:17:46 +0100

    Programming Assignment2- Lexical Scoping
    R function to cache the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,56 @@
 ##Cache the inverse of a matrix
 
-#1. Function to create 'special' matrix to cache its inverse
-makeCacheMatrix <- function(mtx = matrix()){ #assuming symmetric matrix
+##1. Function to create 'special' matrix to cache its inverse
+## assuming symmetric matrix otherwise function will throw error.
+makeCacheMatrix <- function(mtx = matrix()){ 
   inv <- NULL
   set <- function(tmp){ 
     mtx <<- tmp
     inv <<- NULL
   }
-  get <- function() mtx                     #get the matrix
-  setinv <- function(solve) inv <<- solve   #set the value of inverse of the matrix using solve function
-  getinv <- function() inv                  #get the value of inverse of the matrix
-  list(set = set, get = get,                #create a list of the assigned values
-       setinv = setinv,
-       getinv = getinv
+  #get the matrix
+  get <- function() mtx 
+  #set the value of inverse of the matrix using solve function
+  setinv <- function(solve) inv <<- solve
+  #get the value of inverse of the matrix
+  getinv <- function() inv 
+  #create a list of the assigned values
+  list(set = set, get = get, setinv = setinv,getinv = getinv)               
 }
 
 #2. Function to comput inverse of 'special' matrix and, in case already calculated, retrieve the cached value
+
 cacheSolve <- function(mtx = matrix(), ...) {
   inv <- mtx$getinv()
-  if(!is.null(inv)){                        #check whether the inverse has been computed
+  #check whether the inverse has been computed
+  if(!is.null(inv)){                        
     message("getting cached data")
     return(inv)
   }
-  inv_mat <- mtx$get()                      
-  inv <- solve(inv_mat, ...)                #else, compute the inverse matrix
+  inv_mat <- mtx$get()
+  #else, compute the inverse matrix
+  inv <- solve(inv_mat, ...)               
   mtx$setinv(inv)
-  inv                                       #Return a matrix that is the inverse of mtx
+  #print the inverse of the matrix
+  inv                                       
 }
+
+## Sample execution
+##mtx= cbind(c(7, 9), c(9, 7))
+##run function makeCacheMatrix
+##a = makeCacheMatrix(mtx)
+##a$get()
+##[,1] [,2]
+##[1,]    7    9
+##[2,]    9    7
+##run the cacheSolve function to cache the matrix inverse
+##cacheSolve(a)
+##[,1]     [,2]
+##[1,] -0.21875  0.28125
+##[2,]  0.28125 -0.21875
+##Rerun the function to check if caching is occuring.
+##cacheSolve(a)
+##getting cached data
+##[,1]     [,2]
+##[1,] -0.21875  0.28125
+##[2,]  0.28125 -0.21875
